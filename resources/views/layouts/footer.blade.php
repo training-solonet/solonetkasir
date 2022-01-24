13e8f5495e553d28d3b545e59170001239b25d24
unknown <dechan1701@gmail.com>
2022-01-24 12:35:42 -0800

    'kasir-solonet'

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 1.1
+    </div>
+    <strong>Copyright &copy; {{ date('Y') }} <a href="/">nama perusahaan</a>.</strong> All rights
+    reserved.
+</footer>
